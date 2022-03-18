1674d84a3c89b17e08d71f74fe52a6baf776ab82
MarkesZks <gabriel.marquesbiel@hotmail.com>
2022-03-18 12:28:01 -0300

    Commit Disctheme

---
+++ script.js
@@ -0,0 +1,27 @@
+function mudarCor1(){
+    document.querySelector("#b_esq").style.backgroundColor = '#2f3136'
+    document.querySelector("#b_centro").style.backgroundColor = '#FFB6C1'
+    document.querySelector("#b_baixo").style.backgroundColor = '#202225'
+    document.querySelector("#b_dir").style.backgroundColor = '#2f3136'
+}
+
+function mudarCor2(){
+    document.querySelector("#b_esq").style.backgroundColor = '#023020'
+    document.querySelector("#b_centro").style.backgroundColor = '#088F8F'
+    document.querySelector("#b_baixo").style.backgroundColor = '#097969'
+    document.querySelector("#b_dir").style.backgroundColor = '#023020'
+}
+
+function mudarCor3(){
+    document.querySelector("#b_esq").style.backgroundColor = '#FFBF00'
+    document.querySelector("#b_centro").style.backgroundColor ='#FF7009'
+    document.querySelector("#b_baixo").style.backgroundColor = '#E49B0F'
+    document.querySelector("#b_dir").style.backgroundColor = '#FFBF00'
+}
+
+function mudarCor4(){
+    document.querySelector("#b_esq").style.backgroundColor = '#4A0404'
+    document.querySelector("#b_centro").style.backgroundColor = '#DC143C'
+    document.querySelector("#b_baixo").style.backgroundColor = '#811331'
+    document.querySelector("#b_dir").style.backgroundColor = '#4A0404'
+}
